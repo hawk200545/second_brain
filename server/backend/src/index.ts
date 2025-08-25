09c70ce647590943ca13a173787887bd9ea507ea
hawk200545 <girisangar2005@gmail.com>
2025-08-25 12:02:57 +0530

    feat: cors

--- server/backend/src/index.ts
+++ server/backend/src/index.ts
@@ -10,11 +10,12 @@ import cors from "cors"
 const app = express();
 
 app.use(
-  cors({
-    origin: "",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "token"],
-  })
+  // cors({
+  //   origin: "",
+  //   methods: ["GET", "POST", "PUT", "DELETE"],
+  //   allowedHeaders: ["Content-Type", "token"],
+  // })
+  cors()
 );
 
 app.use(json());

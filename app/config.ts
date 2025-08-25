ce29909f2a570c6650b02760b1b429b3a2c90cbd
hawk200545 <girisangar2005@gmail.com>
2025-08-25 14:29:26 +0530

    feat: Login and Backend URL

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,11 @@
-const VERCEL_URL = import.meta.env.VITE_VERCEL_URL;
+let VERCEL_URL = import.meta.env.VITE_VERCEL_URL || '';
+
+if (VERCEL_URL && !VERCEL_URL.startsWith('http')) {
+  VERCEL_URL = `https://${VERCEL_URL}`;
+}
+
+if (VERCEL_URL && !VERCEL_URL.endsWith('/')) {
+  VERCEL_URL += '/';
+}
 
 export { VERCEL_URL };

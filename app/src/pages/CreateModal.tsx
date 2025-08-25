fbcf470e19138dcace4586da7eb5ca3ec2882f25
hawk200545 <girisangar2005@gmail.com>
2025-08-25 15:39:48 +0530

    feat: Added timeout for Signup

--- app/src/pages/CreateModal.tsx
+++ app/src/pages/CreateModal.tsx
@@ -1,5 +1,5 @@
-import { useState, useState } from "react";
+// import { useState, useState } from "react";
 
-export default function CreateModal(){
-    
-}
+// export default function CreateModal(){
+
+// }
